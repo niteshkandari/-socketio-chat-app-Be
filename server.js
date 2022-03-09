c5c9a0a01f5d7e657e24aa8f934902bfeead4ad3
nitesh.kandari <nitesh.kandari@azilen.com>
2022-03-09 21:54:02 +0530

    changes in the url

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require("express")();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: "*",
+    origin: ["https://niteshkandari.github.io/socketio-chat-app-fe/"],
   },
 });
 const port = process.env.PORT || 8000;
