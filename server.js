4ca1b6303616892443b27d04894eb3397fc1293e
nitesh.kandari <nitesh.kandari@azilen.com>
2022-03-09 20:12:33 +0530

    added procfile for heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require("express")();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: ["http://niteshkandari.github.io/socketio-chat-app-fe"],
   },
 });
 const port = process.env.PORT || 8000;
